c0a12cbbb209074869e91c7aeeea094db86c107b
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2022-12-29 17:42:29 +0800

    feat: complete gen/updt

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,83 @@
-use std::fs::OpenOptions;
 use std::io::{self, Write};
+use std::process::Command;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use std::{thread, time};
+
+mod vbios;
+use crate::vbios::{VBios, VBiosBuilder};
+
+const FLASH_FLAG: [u8; 5] = [0x5A, 0x5A, 0xA5, 0xA5, 0x01];
+const DRIVER_FLAG: [u8; 5] = [0x5A, 0xA5, 0x5A, 0xA5, 0x01];
 
 fn main() -> io::Result<()> {
-    {
-        let mut output_file = OpenOptions::new()
-            .create(true)
-            .write(true)
-            .truncate(true)
-            .open("./target/debug/vbiospack.exe")?;
-        let mut content = std::fs::read("./target/debug/vbios.exe")?;
-        let size: [u8; 8] = content.len().to_le_bytes();
-        println!("len: {:?}", content.len());
-        for i in size {
-            print!("{:#04x} ", i);
-        }
-        println!("");
-        let mut flag = vec![0x5A, 0x5A, 0xA5, 0xA5, 0x01];
-        flag.extend_from_slice(&size[..4]);
-        content.extend_from_slice(&flag);
-        output_file.write_all(&content)?;
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        println!("USAGE:");
+        println!("    vbios.exe <SUBCOMMAND>");
+        println!("SUBCOMMAND:");
+        println!("    generate  -> generate executable");
+        println!("    update    -> update / flash bios");
+        return Ok(());
     }
-    {
-        let content = std::fs::read("./target/debug/vbiospack.exe")?;
-        for i in &content[content.len() - 4..] {
-            print!("{:#04x} ", i);
-        }
-        println!("");
-        let result = i32::from_le_bytes(
-            (&content[content.len() - 4..]).try_into().unwrap(),
-        );
-        println!("result: {}", result);
-        let index = content
-            .windows(5)
-            .enumerate()
-            .position(|(i, window)| {
-                window == &[0x5A, 0x5A, 0xA5, 0xA5, 0x01]
-                    && i == i32::from_le_bytes(
-                        (&content[i + 5..i + 9]).try_into().unwrap(),
-                    ) as usize
-            })
-            .unwrap();
-        println!("index: {}", index);
+    if args[1] == "generate" {
+        println!("...generate");
+        let vbios = VBiosBuilder::new("./target/release/vbios.exe")
+            .add_bin("./target/release/input.exe", FLASH_FLAG.into())
+            .add_bin("./target/release/insttool64.exe", DRIVER_FLAG.into())
+            .build()?;
+        vbios.write_all("./target/release/vbiospack.exe")?;
+    } else if args[1] == "update" {
+        println!("...update");
+        let vbios = VBios::from(&args[0]);
+        let flash_index = vbios.find_flag(FLASH_FLAG.to_vec());
+        let driver_index = vbios.find_flag(DRIVER_FLAG.to_vec());
+
+        let tmp_dir = std::env::temp_dir().join("vbios");
+        let flash_path = tmp_dir.join("input.exe");
+        let driver_path = tmp_dir.join("insttool64.exe");
+        std::fs::create_dir_all(&tmp_dir)?;
+
+        vbios.export_bin(&flash_path, flash_index + 9..driver_index)?;
+        vbios.export_bin(&driver_path, driver_index + 9..vbios.size())?;
+
+        println!("...initializing");
+        print!("driver: {}", run_process(driver_path));
+        println!("......done");
+        println!("...flashing");
+        print!("flash: {}", run_process(flash_path));
+        println!("......done");
+        std::fs::remove_dir_all(tmp_dir)?;
+    } else {
+        println!("USAGE:");
+        println!("    vbios.exe <SUBCOMMAND>");
+        println!("SUBCOMMAND:");
+        println!("    generate  -> generate executable");
+        println!("    update    -> update / flash bios");
+        return Ok(());
     }
+    println!("...finish");
     Ok(())
+}
+
+fn run_process(cmd: std::path::PathBuf) -> String {
+    let stop = Arc::new(AtomicBool::new(false));
+    let stop_clone = stop.clone();
+    let (tx, rx) = std::sync::mpsc::channel();
+    let t = thread::spawn(move || {
+        let output = Command::new(cmd)
+            .output()
+            .expect("failed to execute process");
+        tx.send(output).unwrap();
+        stop_clone.store(true, Ordering::Relaxed);
+    });
+    let mut loading_char = vec!["\\", "|", "/", "-"].into_iter().cycle();
+    while !stop.load(Ordering::Relaxed) {
+        thread::sleep(time::Duration::from_millis(100));
+        print!("please wait {}\r", loading_char.next().unwrap());
+        std::io::stdout().flush().unwrap();
+    }
+    print!("{:24 }\r", "");
+    t.join().unwrap();
+    String::from_utf8_lossy(&rx.recv().unwrap().stdout).to_string()
 }
---
+++ src/vbios.rs
@@ -0,0 +1,92 @@
+use std::fs::OpenOptions;
+use std::io::{self, Write};
+use std::ops::Range;
+use std::path::Path;
+
+pub struct VBiosBuilder<P: AsRef<Path> + Clone> {
+    root: P,
+    bins: Vec<(P, Vec<u8>)>,
+}
+
+impl<P: AsRef<Path> + Clone> VBiosBuilder<P> {
+    pub fn new(root: P) -> Self {
+        Self { root, bins: vec![] }
+    }
+    pub fn add_bin(mut self, path: P, flag: Vec<u8>) -> Self {
+        self.bins.push((path, flag));
+        self
+    }
+    pub fn build(&self) -> io::Result<VBios> {
+        let mut buf = std::fs::read(&self.root)?;
+        self.bins.clone().into_iter().for_each(|(p, mut f)| {
+            f.extend_from_slice(&buf.len().to_le_bytes()[..4]);
+            let bin = std::fs::read(&p).unwrap_or_else(|e| {
+                panic!("couldn't read: {}. {}", p.as_ref().display(), e)
+            });
+            buf.extend_from_slice(&f);
+            buf.extend_from_slice(&bin);
+        });
+        Ok(VBios::new(buf))
+    }
+}
+
+#[derive(Default)]
+pub struct VBios {
+    buf: Vec<u8>,
+}
+
+impl<P: AsRef<Path>> From<P> for VBios {
+    fn from(path: P) -> Self {
+        Self {
+            buf: std::fs::read(&path).unwrap_or_else(|e| {
+                panic!("couldn't read: {}. {}", path.as_ref().display(), e)
+            }),
+        }
+    }
+}
+
+impl VBios {
+    fn new(buf: Vec<u8>) -> Self {
+        Self { buf }
+    }
+    pub fn size(&self) -> usize {
+        self.buf.len()
+    }
+    pub fn write_all<P: AsRef<Path>>(&self, path: P) -> io::Result<()> {
+        let mut output_file = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(path)?;
+        output_file.write_all(&self.buf)?;
+        Ok(())
+    }
+    pub fn find_flag(&self, flag: Vec<u8>) -> usize {
+        let f_len = flag.len();
+        self.buf
+            .windows(f_len)
+            .enumerate()
+            .position(|(i, window)| {
+                window == flag
+                    && i == u32::from_le_bytes(
+                        // u32 because we use 4 bytes to store size
+                        (&self.buf[i + f_len..i + f_len + 4])
+                            .try_into()
+                            .unwrap(),
+                    ) as usize
+            })
+            .unwrap_or_else(|| panic!("couldn't find flag: {:#04x?}", flag))
+    }
+    pub fn export_bin<P: AsRef<Path>>(
+        &self,
+        path: P,
+        range: Range<usize>,
+    ) -> io::Result<()> {
+        let mut file = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(path)?;
+        Ok(file.write_all(&self.buf[range])?)
+    }
+}
